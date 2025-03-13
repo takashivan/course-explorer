af68ede459a65943b607318a539189f63327ed82
takashivan <takashi.van822@gmail.com>
2025-03-13 14:17:20 +0900

    テスト 2

--- components/course-chart.tsx
+++ components/course-chart.tsx
@@ -11,7 +11,7 @@ import {
   DialogDescription,
 } from "@/components/ui/dialog";
 import { Star, Clock, Heart } from "lucide-react";
-import { useFavorites } from "../app/hooks/favorite";
+import { useFavorites } from "@/app/hooks/favorite";
 import { Button } from "@/components/ui/button";
 
 Chart.register(...registerables);
